1120af1d3f8dac42816ec0af97b948d6917070bc
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2024-09-12 23:10:10 +0300

    first commit

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+export default function Search({search, setSearch, handleSearch}){
+
+
+  return (
+    <div className="search-engine">
+      <input type="text" 
+        className="city-search"
+        placeholder="Enter city name"
+        name="search"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+        />
+      <button className='search-btn' onClick={handleSearch}>Search weather</button>
+
+    </div>
+  )
+}
